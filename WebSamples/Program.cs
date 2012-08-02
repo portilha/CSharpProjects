5cc6d970566ffa477d6f80749ffb0dfaf8b5992b
portilha <portilha@gmail.com>
2012-08-02 23:52:10 +0100

    Added WebSamples
    Added a new projects with my web experiences.

---
+++ WebSamples/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Net.Mail;
+
+namespace WebSamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string siteToLogon = @"";
+
+            //HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create("http://myapp/home.aspx");
+
+            //request.Method = "GET";
+            //request.UseDefaultCredentials = false;
+            //request.PreAuthenticate = true;
+            //// request.Credentials = new NetworkCredential("username", "password", "domain");
+
+            //HttpWebResponse response = (HttpWebResponse)request.GetResponse(); // Raises Unauthorized Exception
+        }
+
+
+        public static bool Login()
+        {
+            string siteToLogin = @"";
+
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create("http://myapp/home.aspx");
+
+            request.Method = "POST";
+            request.UseDefaultCredentials = true;
+            request.PreAuthenticate = true;
+
+            request.Credentials = CredentialCache.DefaultCredentials;
+
+            // Otherwise try
+            // request.Credentials = new NetworkCredential("username", "password", "domain");
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse(); // Raises Unauthorized Exception
+
+
+            return false;
+        }
+
+        public static void LoginEmulatingTheMozilla()
+        {
+            HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create("http://sso.bhmobile.ba/sso/login");
+            req.UserAgent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.0.3705;)";
+            req.Method = "POST";
+            req.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+            req.Headers.Add("Accept-Language: en-us,en;q=0.5");
+            req.Headers.Add("Accept-Encoding: gzip,deflate");
+            req.Headers.Add("Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7");
+            req.KeepAlive = true;
+            req.Headers.Add("Keep-Alive: 300");
+            req.Referer = "http://sso.bhmobile.ba/sso/login";
+
+            req.ContentType = "application/x-www-form-urlencoded";
+
+            String Username = "username";
+            String PassWord = "Password";
+
+            StreamWriter sw = new StreamWriter(req.GetRequestStream());
+            sw.Write("");
+            sw.Close();
+
+            HttpWebResponse response = (HttpWebResponse)req.GetResponse();
+
+
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string tmp = reader.ReadToEnd();
+
+            foreach (Cookie cook in response.Cookies)
+            {
+                tmp += "\n" + cook.Name + ": " + cook.Value;
+            }
+
+        }
+
+    }
+}
--- XmlLinqSamples/Program.cs
+++ XmlLinqSamples/Program.cs
@@ -12,17 +12,83 @@ namespace XmlLinqSamples
     {
         static void Main(string[] args)
         {
+            string path = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "XmlLinqSamples.csproj");
+    
+            XDocument doc = XDocument.Load(path);
 
-            CreateXmlWithXDocument();
+            // XDocument doc = XDocument.Load(@"xmlTest.xml");
 
-            XDocument doc = XDocument.Load(@"xmlTest.xml");
-
-            foreach (var item in doc.DescendantNodes())
+            foreach (var item in doc.Descendants(XName.Get("Reference", doc.Root.GetDefaultNamespace().ToString())))
             {
-                Console.WriteLine(item.ToString());
+                // Console.WriteLine(item.ToString());
             }
 
+            doc.Descendants().Where<XElement>(e => e.Name.LocalName == "" && e.HasElements);
+
+            XElement.Parse(File.ReadAllText(path));
+
+            foreach (var item in doc.Descendants(XName.Get("PostBuildEvent", doc.Root.GetDefaultNamespace().ToString())))
+            {
+                if (item.HasElements) // Check if the node has children nodes.
+                {
+                    Console.WriteLine("Child Elements:");
+
+                    foreach (var childElements in item.Elements())
+                    {
+                        Console.WriteLine(childElements.ToString());
+                    }
+                }
+
+                if (item.HasAttributes) // Check if the node has attributes
+                {
+                    Console.WriteLine("Attributes");
+
+                    foreach (var att in item.Attributes())
+                    {
+                        Console.WriteLine(string.Format("{0} = {1}", att.Name, att.Value));
+                    }
+                }
+
+                if (!string.IsNullOrWhiteSpace(item.Value)) // Check if the node has content
+                {
+                    Console.WriteLine("Content: " + item.Value);
+                }
+            }
+
+
             // Get a set of elements from a given type.
+
+            Console.ReadKey();
+
+        }
+
+        private static void CreateSimpleElementWithNamespace()
+        {
+            XElement element = new XElement(XName.Get("SpecificVersion", "http://schemas.microsoft.com/developer/msbuild/2003"), true);
+            element.ToString();
+
+            StringBuilder sb = new StringBuilder();
+            using (StringWriter sw = new StringWriter(sb))
+            {
+                element.Save(sw);
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+
+        private static void ShowAllComments(XDocument doc)
+        {
+            Console.WriteLine("Comments:");
+            foreach (var item in doc.DescendantNodes())
+            {
+
+
+                if (item is XComment)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+            Console.WriteLine("End of Comments");
         }
 
         /// <summary>
@@ -31,20 +97,20 @@ namespace XmlLinqSamples
         private static void CreateXmlWithXDocument()
         {
             XDocument doc = new XDocument(
-        new XDeclaration("1.0", "utf-8", "yes"),
-        new XComment("A sample xml file"),
-        new XElement("employees",
-        new XElement("employee",
-           new XAttribute("id", 1),
-           new XAttribute("salaried", "false"),
-              new XElement("name", "Gustavo Achong"),
-              new XElement("hire_date", "7/31/1996")),
-        new XElement("employee",
-           new XAttribute("id", 3),
-           new XAttribute("salaried", "true"),
-              new XElement("name", "Kim Abercrombie"),
-              new XElement("hire_date", "12/12/1997"))
-          ));
+                       new XDeclaration("1.0", "utf-8", "yes"),
+                       new XComment("A sample xml file"),
+                       new XElement("employees",
+                       new XElement("employee",
+                          new XAttribute("id", 1),
+                          new XAttribute("salaried", "false"),
+                             new XElement("name", "Gustavo Achong"),
+                             new XElement("hire_date", "7/31/1996")),
+                       new XElement("employee",
+                          new XAttribute("id", 3),
+                          new XAttribute("salaried", "true"),
+                             new XElement("name", "Kim Abercrombie"),
+                             new XElement("hire_date", "12/12/1997"))
+                         ));
 
             Debug.WriteLine(Directory.GetCurrentDirectory());
 
